1d6c2525f3a53e1505989f6c9d99c75fcde657dd
Fx <82320201+JustFxDev@users.noreply.github.com>
2023-01-05 22:20:42 +0100

    Update translations.php

--- Locale/de_DE/translations.php
+++ Locale/de_DE/translations.php
@@ -3,5 +3,5 @@ return array(
   //
   // GENERAL
   //
-  'A colorful theme to refresh the interface using different color schemes.' => 'Ein farbenfrohes Thema, um die Benutzeroberfläche mit verschiedenen Farbschemata aufzufrischen.',
+  'A colorful theme to refresh the interface using different color schemes.' => 'Ein Kanboard Theme in verschiedenen Farbvarianten und mit Optionen zur Colorierung.',
 );
